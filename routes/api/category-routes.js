19304c5d224219210fb0eaa7507c116eaddd2778
monysary <sary.mony@gmail.com>
2022-12-17 15:43:38 -0800

    Added routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,26 +6,43 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll().then((categoryData) => {
-    res.json(categoryData)
-  })
+  Category.findAll({ include: [Product] })
+    .then((allCategoryData) => {
+      res.json(allCategoryData)
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, { include: [Product] })
+    .then((oneCategoryData) => {
+      res.json(oneCategoryData)
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, { where: { id: req.params.id } })
+    .then((updatedCategory) => {
+      res.json(updatedCategory)
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({ where: { id: req.params.id } })
+    .then((deletedCategory) => {
+      res.json(deletedCategory)
+    })
 });
 
 module.exports = router;
