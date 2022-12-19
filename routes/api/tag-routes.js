c3db515f5d00e1f0e7d0e8d492b41807f3afabd3
monysary <sary.mony@gmail.com>
2022-12-19 13:56:26 -0800

    coded routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,49 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { create } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({include: [Product]})
+    .then((allTag) => {
+      res.json(allTag)
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id)
+    .then((oneTag) => {
+      res.json(oneTag)
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((createdTag) => {
+      res.json(createdTag)
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {where: { id: req.params.id }})
+    .then((updatedTag) => {
+      res.json(updatedTag)
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({where: { id: req.params.id }})
+    .then((deletedTag) => {
+      res.json(deletedTag)
+    })
 });
 
 module.exports = router;
